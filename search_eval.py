3317f971a5ed478a38dac86101186cc97376c707
amithasandur <asandur2@illinois.edu>
2020-09-26 01:45:14 -0500

    update_3

--- search_eval.py
+++ search_eval.py
@@ -36,6 +36,7 @@ def load_ranker(cfg_file):
     configuration file used to load the index. You can ignore this for MP2.
     """
     return InL2Ranker(some_param=1.0)
+    # return metapy.index.OkapiBM25(k1=1.2,b=0.75,k3=500)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
